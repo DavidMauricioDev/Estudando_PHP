b4717f77d111814a49b097b64e7b4796f2e98424
David Mauricio <117051183+DavidMauricioDev@users.noreply.github.com>
2025-10-09 15:16:17 -0300

    Add files via upload

---
+++ concatenacao.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concatenação</title>
+</head>
+<body>
+
+<?php
+$nome ='David';
+$atividade_preferida = 'jogar video game';
+
+    echo "Olá $nome, vi que sua cor preferida é preta e que você gosta de $atividade_preferida"
+
+?>
+    
+</body>
+</html>
---
+++ variaveis.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis</title>
+</head>
+<body>
+   <?php
+   //string
+    $nome = 'David';
+    //int
+    $idade = 30;
+    //float
+    $peso = 89.6;
+    //boolean
+    $fumante_sn = false;
+    ?>
+    
+
+    <h1>Ficha Cadastral</h1>
+    <br/>
+    <p>Nome: <?=$nome ?></p>
+    <p>idade: <?=$idade ?></p>
+    <p>Peso: <?=$peso ?></p>
+    <p>Fumante: <?=$fumante_sn ?></p>
+</body>
+</html>
