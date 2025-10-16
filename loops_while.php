782ae1bfd466e40284713afdd8b331564e47b904
David Mauricio <117051183+DavidMauricioDev@users.noreply.github.com>
2025-10-16 14:57:06 -0300

    Add files via upload

---
+++ loops_while.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+      
+     $num=1;
+     echo '--Inicio do loop--<br/>';
+     while($num < 100){
+        
+        echo "$num  <br/>";
+        $num+=5;
+     }
+
+     echo '--Fim do loop--';
+    
+    
+    
+    
+    
+    ?>
+</body>
+</html>
