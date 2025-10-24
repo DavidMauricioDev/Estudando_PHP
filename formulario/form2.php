c2524b5ab19c0b1ae266867490c132bcad9ae129
David Mauricio <117051183+DavidMauricioDev@users.noreply.github.com>
2025-10-24 14:56:31 -0300

    Add files via upload

--- formulario/form2.php
+++ formulario/form2.php
@@ -1,12 +1,21 @@
+
+
 <?php
-$nome =$_GET["nome"] ;
+$nome = $_GET["nome"];
 $ano = $_GET["ano"];
 $sexo = $_GET["sexo"];
 $idade = date("Y") - $ano;
-echo "$nome tem $idade anos;
 
-
-
-
+echo "Olá $nome </br>";  
+echo "Sua idade é: $idade e seu sexo é $sexo";
 
 ?>
+</br>
+<a href="form2.html">Voltar</a>
+
+
+
+
+
+
+
